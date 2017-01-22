b39710b8e757d91ea1f6ae93f4538fc145d824b2
Flugmango <borisstoecker@gmx.de>
2017-01-22 15:57:13 +0100

    Added countries.json well formatted locally. Extension of URI by type,added switch block for handling type, querying temperature, country code and full country name

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var fs = require('fs');
+var countriesJSON = require('./js/countries.json')
+/*var getFlag = require('../js/getFlag');*/
 
 var app = express();
 
@@ -16,26 +19,58 @@ app.listen(3000, function () {
 
 /* Routes */
 
-app.get('/getData/:lat/:lon', function (req, res) {
+app.get('/getData/:lat/:lon/:type', function (req, res) {
     // Parse our values as integer
     var lat = parseFloat(req.params.lat);
     var lon = parseFloat(req.params.lon);
+    var type = req.params.type;
 
     // Return bad request if coordinates are no numbers
     if (isNaN(lat) || isNaN(lon)) {
         console.log('Incorrect params');
         res.sendStatus(400);
     }
-
-    // Contact OpenWeatherMap API
-    var weatherString = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + appid;
-    request(weatherString, function (error, response, body) {
+    var code = 'http://scatter-otl.rhcloud.com/location?lat=' + lat + '&long=' + lon;
+    request(code, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.json(body);
-        } else {
-            console.log(error);
-            res.sendStatus(500);
+            //country code
+            var countryCode = JSON.parse(body).countrycode;
+            console.log(countryCode);
+            //full country name
+            var countryName = find_in_object(countriesJSON, { code: countryCode });
+            console.log(countryName[0].name);
+            //data queries depending on type specified in URI
+            switch (type) {
+                case "temperature":
+                    // Contact OpenWeatherMap API
+                    var weatherString = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + appid;
+                    request(weatherString, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            var data = JSON.parse(body);
+                            var temp = data.main.temp;
+                            res.json((parseFloat(temp) - 273.15).toFixed(2) + " °C");
+                        } else {
+                            console.log(error);
+                            res.sendStatus(500);
+                        }
+                    });
+                    break;
+                case "population":
+                    /** TODO */
+                    break;
+            };
         }
-    })
+    });
 
-})
+
+    // help function for filtering json
+    function find_in_object(my_object, my_criteria) {
+
+        return my_object.filter(function (obj) {
+            return Object.keys(my_criteria).every(function (c) {
+                return obj[c] == my_criteria[c];
+            });
+        });
+
+    };
+});
